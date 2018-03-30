c31c7ba8b7581b9c181636341cacf46ca3016bd9
Narasimha1997 <yogaimpression@gmail.com>
2018-03-30 11:12:53 +0530

    Hello

---
+++ index.js
@@ -0,0 +1,48 @@
+var express = require('express')
+
+var bp = require('body-parser')
+
+var request = require('request')
+
+let URL = 'http://api.wolframalpha.com/v1/spoken'
+
+app = express()
+app.use(bp.json())
+
+app.post("/api/login", (req, resp) => {
+    let data = req.body
+    resp.setHeader('Content-Type', 'application/json')
+    if(data.name == 'Prasanna' && data.password == 'Prasanna123'){
+        resp.send(JSON.stringify({
+            success : true
+        }))
+    }else{
+        resp.send(JSON.stringify({
+            success : false
+        }))
+    }
+})
+
+function send_response(res, body, resp){
+    if(body!=null){
+        resp.setHeader('Content-Type', 'application/json')
+        resp.send(JSON.stringify({
+            success : true,
+            content : body
+        }))
+    }
+}
+
+app.post('/api/query', (req, resp) => {
+    let query = req.body.query_string
+    if(query!= null){
+        console.log(query)
+        request.get({url : URL, qs : {'appid' : '6JKYLV-4YG98RJUPV', 'i' : query}}, (err, res, body) =>{
+               send_response(res, body, resp)
+        })
+      }
+    })
+
+app.listen(3000, '192.168.0.103', ()=>{
+    console.log('Server running')
+})
